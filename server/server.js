dea54eb153fe9fbd150a7d2cd30b074c373d21e4
Jason Li <JasonLiSoftware@gmail.com>
2024-03-25 15:51:31 -0400

    init

---
+++ server/server.js
@@ -0,0 +1,13 @@
+import http from "http";
+import dotenv from "dotenv";
+
+dotenv.config({ path: "./config.env" });
+
+const server = http.createServer((req, res) => {
+  res.end("Connected");
+});
+
+const port = process.env.PORT || 3000;
+server.listen(port, (req, res) => {
+  console.log(`Server listening on port ${port}`);
+});
